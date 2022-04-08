a568eb7279ac7b4e86a382acd4819fec25c4f22c
Jovke02 <urosjovicic6@gmail.com>
2022-04-08 23:15:44 +0200

    Java klasa za menadzera

---
+++ menadzer.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Date;
+
+public class Menadzer extends Korisnik{
+    private String Restoran;
+
+    public Menadzer(String korisnicko_ime, String lozinka, String ime, String prezime, pol p, Date datum_rodjenja) {
+        super(korisnicko_ime, lozinka, ime, prezime, p, datum_rodjenja);
+    }
+
+    public void setRestoran(String restoran) {
+        Restoran = restoran;
+    }
+
+    @Override
+    public String toString() {
+        return "Menadzer je zaduzen za restoran: " + Restoran + ".\n";
+    }
+}
