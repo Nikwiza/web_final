6d1c48a454dbea90eecb8144fe9a560270fa6308
Jovke02 <urosjovicic6@gmail.com>
2022-04-11 13:40:39 +0200

    Azurirana java klasa restoran

--- src/main/java/com/company/entity/Restoran.java
+++ src/main/java/com/company/entity/Restoran.java
@@ -1,10 +1,30 @@
 package com.company.entity;
 import java.util.ArrayList;
+import java.io.Serializable;
+import javax.persistence.*;
 
-public class Restoran {
+@Entity
+@NamedQuery(name = "Restoran.findALL", query = "SELECT r FROM Restoran r")
+public class Restoran implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idLokacija;
+
+    @Column
     private String naziv;
+
+    @Column
     private String tip_restorana;
+
+    @Column
     private ArrayList<Artikal> artikli;
+
+    public Lokacija(){}
+
+    @OneToOne
+    @JoinColumn(name = "idLokacija")
     private Lokacija lokacija;
 
     public Restoran(String naziv, String tip_restorana, ArrayList<Artikal> artikli, Lokacija lokacija) {
@@ -12,6 +32,14 @@ public class Restoran {
         this.tip_restorana = tip_restorana;
         this.artikli = artikli;
         this.lokacija = lokacija;
+    }
+
+    public int getIdLokacija() {
+        return this.idLokacija;
+    }
+
+    public void setIdLokacija(int idLokacija) {
+        this.idLokacija = idLokacija;
     }
 
     public String getNaziv() {
