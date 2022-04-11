30b3a05dfcdb3cdd900ad42e3e651604188138ad
Jovke02 <urosjovicic6@gmail.com>
2022-04-11 13:32:05 +0200

    Azurirana java klasa lokacija

--- src/main/java/com/company/entity/Lokacija.java
+++ src/main/java/com/company/entity/Lokacija.java
@@ -1,15 +1,51 @@
 package com.company.entity;
 //Funckija koja predstavlja lokaciju
+import java.io.Serializable;
+import javax.persistence.*;
 
-public class Lokacija {
+@Entity
+@NamedQuery(name = "Lokacija.findALL", query = "SELECT u FROM Lokacija l")
+public class Lokacija implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idLokacija;
+
+    @Column
     protected double geografska_duzina;
+
+    @Column
     protected double geografska_sirina;
+
+    @Column
     protected String adresa;
+
+    public Lokacija() {}
+
+    @OneToOne(mappedBy = "lokacija")
+    private Restoran restoran;
+
+    public Restoran getRestoran() {
+        return this.restoran;
+    }
+
+    public void setRestoran(Restoran restoran) {
+        this.restoran = restoran;
+    }
 
     public Lokacija(double geografska_duzina, double geografska_sirina, String adresa) {
         this.geografska_duzina = geografska_duzina;
         this.geografska_sirina = geografska_sirina;
         this.adresa = adresa;
+    }
+
+    public int getIdLokacija(){
+        return this.idLokacija;
+    }
+
+    public void setIdLokacija(int idLokacija){
+        this.idLokacija = idLokacija;
     }
 
     public double getGeografska_duzina() {
