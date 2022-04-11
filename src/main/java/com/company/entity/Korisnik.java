dfd52a69541bcfe27deb373ef95bf1c77f815e6c
Jovke02 <urosjovicic6@gmail.com>
2022-04-11 12:50:06 +0200

    Azurirana java klasa korisnik

--- src/main/java/com/company/entity/Korisnik.java
+++ src/main/java/com/company/entity/Korisnik.java
@@ -2,13 +2,16 @@ package com.company.entity;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.*;
 import java.io.Serializable;
 
 enum uloga{Admin, Menadzer, Dostavljac, Kupac};
 enum pol{MUSKI, ZENSKI};
 @Entity
+@NamedQuery(name = "Korisnik.findALL", query = "SELECT u FROM Korisnik k")
 public class Korisnik implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerrationType.IDENTITY)
     private Long idKorisnika;
@@ -34,6 +37,80 @@ public class Korisnik implements Serializable {
     @Enumerated(EnumType.ORDINAL)
 
     public Korisnik(){}
+
+    @OneToMany(mappedBy = "korisnik")
+    private List<Kupac> kupci;
+
+    @OneToMany(mappedBy = "korisnik")
+    private List<Dostavljac> dostavljaci;
+
+    @OneToMany(mappedBy = "korisnik")
+    private List<Menadzer> menadzeri;
+
+    //dodatak za dostavljaca(get, set, add, remove)
+    public List<Dostavljac> getDostavljaci() {
+        return this.dostavljaci;
+    }
+
+    public void setDostavljaci(List<Dostavljac> dostavljaci) {
+        this.dostavljaci = dostavljaci;
+    }
+
+    public Dostavljac addDostavljac(Dostavljac dostavljac){
+        getDostavljaci().add(dostavljac);
+        dostavljac.setKorisnik(this);
+        return dostavljac;
+    }
+
+    public Dostavljac removeDostavljac(Dostavljac dostavljac){
+        getDostavljaci().remove(dostavljac);
+        dostavljac.setKorisnik(null);
+        return dostavljac;
+    }
+
+    //dodatak za kupca(get, set, add, remove)
+
+
+    public List<Kupac> getKupci() {
+        return this.kupci;
+    }
+
+    public void setKupci(List<Kupac> kupci) {
+        this.kupci = kupci;
+    }
+
+    public Kupac addKupac(Kupac kupac){
+        getKupci().add(kupac);
+        kupac.setKorisnik(this);
+        return kupac;
+    }
+
+    public Kupac removeKupac(Kupac kupac){
+        getKupci().remove(kupac);
+        kupac.setKupac(null);
+        return kupac;
+    }
+
+    //dodatak za menadzera(get, set, add, remove)
+    public List<Menadzer> getMenadzeri() {
+        return this.menadzeri;
+    }
+
+    public void setMenadzeri(List<Menadzer> menadzeri) {
+        this.menadzeri = menadzeri;
+    }
+
+    public Menadzer addMenadzer(Menadzer menadzer){
+        getMenadzeri().add(menadzer);
+        menadzer.setKorisnik(this);
+        return menadzer;
+    }
+
+    public Menadzer removeMenadzer(Menadzer menadzer){
+        getMenadzeri().remove(menadzer);
+        menadzer.setKorisnik(null);
+        return menadzer;
+    }
 
     public Korisnik(String korisnicko_ime, String lozinka, String ime, String prezime, Pol p, Date datum_rodjenja) {
         this.korisnicko_ime = korisnicko_ime;
