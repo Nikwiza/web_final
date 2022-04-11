df636b220a1b0fea3d909eff8700be22bec84ca7
Jovke02 <urosjovicic6@gmail.com>
2022-04-11 12:11:53 +0200

    Azurirana java klasa menadzer

--- src/main/java/com/company/entity/Menadzer.java
+++ src/main/java/com/company/entity/Menadzer.java
@@ -1,9 +1,35 @@
 package com.company.entity;
 
 import java.util.Date;
+import java.io.Serializable;
+import javax.persistence.*;
 
-public class Menadzer extends Korisnik{
+@Entity
+@NamedQuery(name = "Menadzer.findALL", query = "SELECT u FROM Menadzer m")
+
+public class Menadzer implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMenadzer;
+
+    public Menadzer(){}
+
+    @Column
     private String Restoran;
+
+    public int getIdMenadzer() {
+        return idMenadzer;
+    }
+
+    public void setIdMenadzer(int idMenadzer) {
+        this.idMenadzer = idMenadzer;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "idKorisnik")
+    private Korisnik korisnik;
 
     public Menadzer(String korisnicko_ime, String lozinka, String ime, String prezime, Pol p, Date datum_rodjenja) {
         super(korisnicko_ime, lozinka, ime, prezime, p, datum_rodjenja);
