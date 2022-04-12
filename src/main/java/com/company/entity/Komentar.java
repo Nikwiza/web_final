17efae50b87433395da422779f7cffddd26b5e3b
Jovke02 <urosjovicic6@gmail.com>
2022-04-12 12:08:44 +0200

    Azuriranje java klase komentar

--- src/main/java/com/company/entity/Komentar.java
+++ src/main/java/com/company/entity/Komentar.java
@@ -1,23 +1,91 @@
 package com.company.entity;
 
-
+import java.io.Serializable;
+import javax.persistence.*;
 import java.rmi.UnexpectedException;
+import java.util.List;
 
 // Klasa koja predstavlja Komentare
+@Entity
+@NamedQuery(name = "Komentar.findALL", query = "SELECT k FROM Komentar k")
+public class Komentar implements Serializable{
+    private static final long serialVersionUID = 1L;
 
-public class Komentar{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idKomentar;
+
+    @Column
     private Kupac kupac;
+
+    @Column
     private Restoran restoran;
+
+    @Column
     private String text;
+
+    @Column
     private int ocena;
 
+    public Komentar(){}
 
-    public Kupac getKupac() {
+    @OneToMany(mappedBy = "komentar")
+    private List<Kupac> kupci;
+
+    @OneToMany(mappedBy = "komentar")
+    private List<Restoran> restorani;
+
+    public int getIdKomentar() {
+        return this.idKomentar;
+    }
+
+    public void setIdKomentar(int idKomentar) {
+        this.idKomentar = idKomentar;
+    }
+
+
+    //dodatak za kupca(set, get, add, remove)
+    public List<Kupac> getKupci() {
+        return this.kupci;
+    }
+
+    public void setKupac(List<Kupac> kupac) {
+        this.kupac = kupac;
+    }
+
+    public Kupac addKupac(Kupac kupac){
+        getKupci().add(kupac);
+        kupac.setKomentar(this);
         return kupac;
     }
 
-    public void setKupac(Kupac kupac) {
-        this.kupac = kupac;
+    public Kupac removeKupac(Kupac kupac){
+        getKupci().remove(kupac);
+        kupac.setKomentar(null);
+        return kupac;
+    }
+
+    //dodatak za restoran(set, get, add, remove)
+
+
+    public List<Restoran> getRestorani() {
+        return this.restorani;
+    }
+
+    public void setRestorani(List<Restoran> restorani) {
+        this.restorani = restorani;
+    }
+
+    public Restoran addRestoran(Restoran restoran){
+        getRestorani().add(restoran);
+        restoran.setKomentar(this);
+        return resstoran;
+    }
+
+    public Restoran removeRestoran(Restoran restoran){
+        getRestorani().remove(restoran);
+        restoran.setKomentar(null);
+        return restoran;
     }
 
     public Restoran getRestoran() {
